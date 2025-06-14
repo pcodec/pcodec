ab89a154c36e82ccc79a34941034155ac96e8bc0
Niels Jeppesen <niels@jeppesen.dk>
2025-06-14 12:56:58 +0200

    ...

--- pco/src/ans/mod.rs
+++ pco/src/ans/mod.rs
@@ -57,7 +57,7 @@ mod tests {
     let mut state_idx = final_state - table_size;
     for _ in 0..symbols.len() {
       let node = &decoder.nodes[state_idx as usize];
-      decoded.push(node.symbol as Symbol);
+      decoded.push(node.symbol);
       state_idx = node.next_state_idx_base as AnsState
         + unsafe { reader.read_uint::<AnsState>(node.bits_to_read as Bitlen) };
     }
