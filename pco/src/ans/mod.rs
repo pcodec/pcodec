9adace16c0f9b8357bb4f77b8564e751ba80a912
Niels Jeppesen <niels@jeppesen.dk>
2025-06-16 00:17:02 +0200

    Revert "Fix test"
    This reverts commit 42b13221b595900e208c04a231c33b92fe407a29.

--- pco/src/ans/mod.rs
+++ pco/src/ans/mod.rs
@@ -58,9 +58,9 @@ mod tests {
     let mut state_idx = final_state - table_size;
     for _ in 0..symbols.len() {
       let node = &decoder.nodes[state_idx as usize];
-      decoded.push(node.symbol as Symbol);
+      decoded.push(node.symbol);
       state_idx = node.next_state_idx_base as AnsState
-        + unsafe { reader.read_uint::<AnsState>(node.bits_to_read) };
+        + unsafe { reader.read_uint::<AnsState>(node.bits_to_read as Bitlen) };
     }
 
     assert_eq!(decoded, symbols);
