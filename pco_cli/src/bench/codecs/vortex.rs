548197138973a91788cc80a4241aa5a326ced031
mwlon <m.w.loncaric@gmail.com>
2025-09-19 10:13:02 -0400

    almost working
    Signed-off-by: mwlon <m.w.loncaric@gmail.com>

--- pco_cli/src/bench/codecs/vortex.rs
+++ pco_cli/src/bench/codecs/vortex.rs
@@ -1,25 +1,35 @@
 use once_cell::sync::Lazy;
+use vortex::compressor::CompactCompressor;
 
 use crate::bench::codecs::{utils, CodecInternal};
 use crate::dtypes::PcoNumber;
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use pco::data_types::NumberType;
 use pco::match_number_enum;
 use tokio::runtime::Runtime;
 use vortex::arrays::PrimitiveArray;
 use vortex::buffer::ByteBuffer;
 use vortex::dtype::DType;
-use vortex::file::{VortexOpenOptions, VortexWriteOptions};
+use vortex::file::{VortexOpenOptions, VortexWriteOptions, WriteStrategyBuilder};
 use vortex::scalar::ScalarType;
-use vortex::stream::ArrayStreamArrayExt;
+use vortex::stream::ArrayStreamExt;
 use vortex::validity::Validity;
 use vortex::ToCanonical;
 
 static RUNTIME: Lazy<Runtime> =
   Lazy::new(|| Runtime::new().expect("Failed to create Tokio runtime"));
 
+#[derive(Clone, Copy, Debug, ValueEnum)]
+pub enum Strategy {
+  Btrblocks,
+  Compact,
+}
+
 #[derive(Clone, Debug, Parser)]
-pub struct VortexConfig {}
+pub struct VortexConfig {
+  #[arg(long, default_value = "btrblocks")]
+  strategy: Strategy,
+}
 
 impl CodecInternal for VortexConfig {
   fn name(&self) -> &'static str {
@@ -27,7 +37,10 @@ impl CodecInternal for VortexConfig {
   }
 
   fn get_confs(&self) -> Vec<(&'static str, String)> {
-    vec![]
+    vec![(
+      "strategy",
+      format!("{:?}", self.strategy).to_lowercase(),
+    )]
   }
 
   fn compress<T: PcoNumber>(&self, nums: &[T]) -> Vec<u8> {
@@ -45,21 +58,36 @@ impl CodecInternal for VortexConfig {
     };
     let vortex_arr = PrimitiveArray::from_byte_buffer(byte_buffer, ptype, Validity::NonNullable);
 
+    let mut strategy = WriteStrategyBuilder::default();
+    match self.strategy {
+      Strategy::Btrblocks => (),
+      Strategy::Compact => strategy = strategy.with_compressor(CompactCompressor::default()),
+    };
+    let options = VortexWriteOptions::default().with_strategy(strategy.build());
+
     // unfortunately vortex only has an async API
-    // By default, writing an array will decompress it back to its canonical form and then recompress it, so there's no need to compress it ahead of time.
-    RUNTIME
-      .block_on(VortexWriteOptions::default().write(Vec::new(), vortex_arr.to_array_stream()))
-      .expect("vortex failed to write")
+    // By default, writing an array will decompress it back to its canonical
+    // form and then recompress it, so there's no need to compress it ahead of
+    // time.
+    let res = RUNTIME
+      .block_on(options.write(Vec::new(), vortex_arr.to_array_stream()))
+      .expect("vortex failed to write");
+    println!(
+      "compressed to {:?} with {:?}",
+      res.len(),
+      self.strategy
+    );
+    res
   }
 
   fn decompress<T: PcoNumber>(&self, src: &[u8]) -> Vec<T> {
     // again, we must copy
-    let x = RUNTIME
-      .block_on(VortexOpenOptions::in_memory().open(ByteBuffer::from(src.to_vec())))
+    let file = VortexOpenOptions::in_memory()
+      .open(ByteBuffer::from(src.to_vec()))
       .unwrap();
     let mut res = vec![];
-    for array_result in x.scan().unwrap().into_array_iter().unwrap() {
-      let array = array_result.unwrap().to_primitive().unwrap();
+    for array_result in file.scan().unwrap().into_array_iter().unwrap() {
+      let array = array_result.unwrap().to_primitive();
       res.extend(array.buffer::<T>());
     }
     res
