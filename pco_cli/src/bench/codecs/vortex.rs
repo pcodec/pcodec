3dd7f5a230a8b5d7bda8efd75d9b5495026208cb
Martin <m.w.loncaric@gmail.com>
2025-09-26 08:57:52 -0400

    Update vortex dependency and add Compact strategy to bench (#324)

--- pco_cli/src/bench/codecs/vortex.rs
+++ pco_cli/src/bench/codecs/vortex.rs
@@ -1,25 +1,33 @@
 use once_cell::sync::Lazy;
+use vortex::compressor::CompactCompressor;
+use vortex::io::runtime::current::CurrentThreadRuntime;
 
-use crate::bench::codecs::{utils, CodecInternal};
+use crate::bench::codecs::CodecInternal;
 use crate::dtypes::PcoNumber;
-use clap::Parser;
-use pco::data_types::NumberType;
-use pco::match_number_enum;
+use clap::{Parser, ValueEnum};
 use tokio::runtime::Runtime;
 use vortex::arrays::PrimitiveArray;
-use vortex::buffer::ByteBuffer;
-use vortex::dtype::DType;
-use vortex::file::{VortexOpenOptions, VortexWriteOptions};
-use vortex::scalar::ScalarType;
-use vortex::stream::ArrayStreamArrayExt;
+use vortex::buffer::{Buffer, ByteBuffer};
+use vortex::file::{VortexOpenOptions, VortexWriteOptions, WriteStrategyBuilder};
+use vortex::io::runtime::tokio::TokioRuntime;
+use vortex::stream::ArrayStreamExt;
 use vortex::validity::Validity;
 use vortex::ToCanonical;
 
 static RUNTIME: Lazy<Runtime> =
   Lazy::new(|| Runtime::new().expect("Failed to create Tokio runtime"));
 
+#[derive(Clone, Copy, Debug, ValueEnum)]
+pub enum Strategy {
+  Btrblocks,
+  Compact,
+}
+
 #[derive(Clone, Debug, Parser)]
-pub struct VortexConfig {}
+pub struct VortexConfig {
+  #[arg(long, default_value = "btrblocks")]
+  strategy: Strategy,
+}
 
 impl CodecInternal for VortexConfig {
   fn name(&self) -> &'static str {
@@ -27,41 +35,61 @@ impl CodecInternal for VortexConfig {
   }
 
   fn get_confs(&self) -> Vec<(&'static str, String)> {
-    vec![]
+    vec![(
+      "strategy",
+      format!("{:?}", self.strategy).to_lowercase(),
+    )]
   }
 
   fn compress<T: PcoNumber>(&self, nums: &[T]) -> Vec<u8> {
-    // can't figure out a way to avoid copying here
-    let byte_buffer = ByteBuffer::copy_from(unsafe { utils::num_slice_to_bytes(nums) });
-    let number_type = NumberType::from_descriminant(T::NUMBER_TYPE_BYTE).unwrap();
-    let dtype = match_number_enum!(number_type,
-      NumberType<T> => {
-        T::dtype()
-      }
+    let vx_arr = PrimitiveArray::new(
+      // can't figure out a way to avoid copying the numbers here
+      Buffer::copy_from(nums),
+      Validity::NonNullable,
     );
-    let ptype = match dtype {
-      DType::Primitive(ptype, _) => ptype,
-      _ => unreachable!(),
+
+    let mut strategy = WriteStrategyBuilder::default();
+    match self.strategy {
+      Strategy::Btrblocks => (),
+      Strategy::Compact => strategy = strategy.with_compressor(CompactCompressor::default()),
     };
-    let vortex_arr = PrimitiveArray::from_byte_buffer(byte_buffer, ptype, Validity::NonNullable);
+    let options = VortexWriteOptions::default()
+      .with_strategy(strategy.build())
+      .blocking::<CurrentThreadRuntime>();
 
     // unfortunately vortex only has an async API
-    // By default, writing an array will decompress it back to its canonical form and then recompress it, so there's no need to compress it ahead of time.
-    RUNTIME
-      .block_on(VortexWriteOptions::default().write(Vec::new(), vortex_arr.to_array_stream()))
-      .expect("vortex failed to write")
+    // By default, writing an array will decompress it back to its canonical
+    // form and then recompress it, so there's no need to compress it ahead of
+    // time.
+    let mut res = Vec::new();
+    options
+      .write(&mut res, vx_arr.to_array_iterator())
+      .expect("vortex failed to write");
+    res
   }
 
   fn decompress<T: PcoNumber>(&self, src: &[u8]) -> Vec<T> {
-    // again, we must copy
-    let x = RUNTIME
-      .block_on(VortexOpenOptions::in_memory().open(ByteBuffer::from(src.to_vec())))
+    let handle = TokioRuntime::current();
+    let file = RUNTIME
+      .block_on(
+        VortexOpenOptions::new()
+          .with_handle(handle.clone())
+          // again, we must copy the compressed data
+          .open(ByteBuffer::copy_from(src)),
+      )
       .unwrap();
-    let mut res = vec![];
-    for array_result in x.scan().unwrap().into_array_iter().unwrap() {
-      let array = array_result.unwrap().to_primitive().unwrap();
-      res.extend(array.buffer::<T>());
-    }
-    res
+    let vx_arr = RUNTIME
+      .block_on(
+        file
+          .scan()
+          .unwrap()
+          .with_handle(handle)
+          .into_array_stream()
+          .unwrap()
+          .read_all(),
+      )
+      .unwrap();
+    // aaand one last copy for the numbers
+    vx_arr.to_primitive().buffer::<T>().to_vec()
   }
 }
