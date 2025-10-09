4a21ba8688a27ad6e21ca31bff717070422aa34f
Martin <m.w.loncaric@gmail.com>
2025-10-09 10:03:53 -0400

    OpenZL comparator (#328)

---
+++ pco_cli/src/bench/codecs/openzl.rs
@@ -0,0 +1,24 @@
+use crate::bench::codecs::CodecInternal;
+use crate::dtypes::PcoNumber;
+use clap::Parser;
+
+#[derive(Clone, Debug, Parser)]
+pub struct OpenZlConfig {}
+
+impl CodecInternal for OpenZlConfig {
+  fn name(&self) -> &'static str {
+    "openzl"
+  }
+
+  fn get_confs(&self) -> Vec<(&'static str, String)> {
+    vec![]
+  }
+
+  fn compress<T: PcoNumber>(&self, nums: &[T]) -> Vec<u8> {
+    rust_openzl::compress_numeric(nums).unwrap()
+  }
+
+  fn decompress<T: PcoNumber>(&self, bytes: &[u8]) -> Vec<T> {
+    rust_openzl::decompress_numeric(bytes).unwrap()
+  }
+}
