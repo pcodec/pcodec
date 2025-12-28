b61a1a5c1b34cc33c72c865a623b7015e6702f37
mwlon <m.w.loncaric@gmail.com>
2025-12-28 15:44:14 -0800

    classic mode file
    Signed-off-by: mwlon <m.w.loncaric@gmail.com>

---
+++ pco/src/mode/classic.rs
@@ -0,0 +1,19 @@
+use crate::{
+  data_types::{Number, SplitLatents},
+  dyn_latent_slice::DynLatentSlice,
+  metadata::DynLatents,
+};
+
+pub(crate) fn split_latents<T: Number>(nums: &[T]) -> SplitLatents {
+  let primary = DynLatents::new(nums.iter().map(|&x| x.to_latent_ordered()).collect());
+  SplitLatents {
+    primary,
+    secondary: None,
+  }
+}
+
+pub(crate) fn join_latents<T: Number>(primary: DynLatentSlice, dst: &mut [T]) {
+  for (&l, num) in primary.downcast_unwrap::<T::L>().iter().zip(dst.iter_mut()) {
+    *num = T::from_latent_ordered(l);
+  }
+}
