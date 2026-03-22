f27b84b063e77491145811cc8a3eaf3816b02741
mwlon <m.w.loncaric@gmail.com>
2026-03-22 08:28:30 -0400

    nit
    Signed-off-by: mwlon <m.w.loncaric@gmail.com>

--- pco_cli/src/bench/codecs/sprintz.rs
+++ pco_cli/src/bench/codecs/sprintz.rs
@@ -29,7 +29,7 @@ impl CodecInternal for SprintzConfig {
   }
 
   fn compress<T: PcoNumber>(&self, nums: &[T]) -> Vec<u8> {
-    let len = nums.len();
+    let len = nums.len() as u32;
     let mut res = Vec::new();
     res.extend((len as u32).to_le_bytes());
 
@@ -37,19 +37,19 @@ impl CodecInternal for SprintzConfig {
     res.reserve(buf_size);
     let pos = res.len();
     let clen = unsafe {
-      let dest = res.as_mut_ptr().add(pos);
+      let dst = res.as_mut_ptr().add(pos);
       match (size_of::<T>(), &self.filter) {
         (1, Filter::Delta) => {
-          sprintz_sys::sprintz_c_compress_delta_8b(nums.as_ptr() as _, len as u32, dest as _, 1)
+          sprintz_sys::sprintz_c_compress_delta_8b(nums.as_ptr() as _, len, dst, 1)
         }
         (1, Filter::Xff) => {
-          sprintz_sys::sprintz_c_compress_xff_8b(nums.as_ptr() as _, len as u32, dest as _, 1)
+          sprintz_sys::sprintz_c_compress_xff_8b(nums.as_ptr() as _, len, dst as _, 1)
         }
         (2, Filter::Delta) => {
-          sprintz_sys::sprintz_c_compress_delta_16b(nums.as_ptr() as _, len as u32, dest as _, 1)
+          sprintz_sys::sprintz_c_compress_delta_16b(nums.as_ptr() as _, len, dst as _, 1)
         }
         (2, Filter::Xff) => {
-          sprintz_sys::sprintz_c_compress_xff_16b(nums.as_ptr() as _, len as u32, dest as _, 1)
+          sprintz_sys::sprintz_c_compress_xff_16b(nums.as_ptr() as _, len, dst as _, 1)
         }
         _ => panic!("sprintz only supports 1- and 2-byte types"),
       }
@@ -61,7 +61,7 @@ impl CodecInternal for SprintzConfig {
     );
     // If we don't add +2 here, the last decompressed number will be wrong.
     // Seems like a bug on their side.
-    unsafe { res.set_len(pos + clen as usize * size_of::<T>() + 2) };
+    unsafe { res.set_len(pos + (clen as usize) * size_of::<T>() + 2) };
     res
   }
 
@@ -71,19 +71,19 @@ impl CodecInternal for SprintzConfig {
 
     let mut out = Vec::<T>::with_capacity(len + 64);
     let dlen = unsafe {
-      let dest = out.as_mut_ptr();
+      let dst = out.as_mut_ptr();
       match (size_of::<T>(), &self.filter) {
         (1, Filter::Delta) => {
-          sprintz_sys::sprintz_c_decompress_delta_8b(compressed.as_ptr() as _, dest as _)
+          sprintz_sys::sprintz_c_decompress_delta_8b(compressed.as_ptr() as _, dst as _)
         }
         (1, Filter::Xff) => {
-          sprintz_sys::sprintz_c_decompress_xff_8b(compressed.as_ptr() as _, dest as _)
+          sprintz_sys::sprintz_c_decompress_xff_8b(compressed.as_ptr() as _, dst as _)
         }
         (2, Filter::Delta) => {
-          sprintz_sys::sprintz_c_decompress_delta_16b(compressed.as_ptr() as _, dest as _)
+          sprintz_sys::sprintz_c_decompress_delta_16b(compressed.as_ptr() as _, dst as _)
         }
         (2, Filter::Xff) => {
-          sprintz_sys::sprintz_c_decompress_xff_16b(compressed.as_ptr() as _, dest as _)
+          sprintz_sys::sprintz_c_decompress_xff_16b(compressed.as_ptr() as _, dst as _)
         }
         _ => panic!("sprintz only supports 1- and 2-byte types"),
       }
